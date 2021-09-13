bb8690f65e250041f9246f5113075a8762d1fac4
Jon Fowler <jfowler27@gmail.com>
2021-09-12 18:02:12 -0600

    Update compatibility with multiple monitors

--- taskbarhider.cpp
+++ taskbarhider.cpp
@@ -1,10 +1,19 @@
 #include <windows.h>
 
+using namespace std;
+
 int main() {
-    HWND hwnd = FindWindow("Shell_traywnd", "");
-    if (IsWindowVisible(hwnd))
-        SetWindowPos(hwnd,0,0,0,0,0,SWP_HIDEWINDOW);
-    else
-        SetWindowPos(hwnd,0,0,0,0,0,SWP_SHOWWINDOW);
+    HWND hwnd = FindWindow(TEXT("Shell_traywnd"), NULL);
+    HWND hwnd2 = FindWindow(TEXT("Shell_SecondaryTrayWnd"), NULL);
+    if (IsWindowVisible(hwnd)) {
+        ShowWindow(hwnd, 0);
+        ShowWindow(hwnd2, 0);
+        //SetWindowPos(hwnd,0,0,0,0,0,SWP_HIDEWINDOW);
+    }
+    else {
+        ShowWindow(hwnd, 1);
+        ShowWindow(hwnd2, 1);
+        //SetWindowPos(hwnd,0,0,0,0,0,SWP_SHOWWINDOW);
+    }
     return 0;
 }
