75976783c781a04be3a1b3143a22400155e93fd2
Jon Fowler <jfowler27@gmail.com>
2024-03-03 08:06:54 -0800

    Clean up project
    -Clean up code and add comments
    -Update .gitignore
    -Update readme with tested versions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # taskbarhider
 
-Small and straightforward C++ program to hide the Windows Taskbar using the Windows API.
+Small C++ program to hide/show the Windows Taskbar using the Windows API. It simply toggles between showing the taskbar or not and attempts to work against multiple monitors.
+
+Seemed to work consistently with Windows 7 and 10. Windows 11 seems to try and recover the main monitor taskbar automatically (as of testing March 2024).
--- taskbarhider.cpp
+++ taskbarhider.cpp
@@ -3,17 +3,19 @@
 using namespace std;
 
 int main() {
+    // Get a handle to the taskbar and secondary taskbar windows
     HWND hwnd = FindWindow(TEXT("Shell_traywnd"), NULL);
     HWND hwnd2 = FindWindow(TEXT("Shell_SecondaryTrayWnd"), NULL);
+
+    // If the taskbar is visible, hide it; otherwise, show it
     if (IsWindowVisible(hwnd)) {
         ShowWindow(hwnd, 0);
         ShowWindow(hwnd2, 0);
-        //SetWindowPos(hwnd,0,0,0,0,0,SWP_HIDEWINDOW);
     }
     else {
         ShowWindow(hwnd, 1);
         ShowWindow(hwnd2, 1);
-        //SetWindowPos(hwnd,0,0,0,0,0,SWP_SHOWWINDOW);
     }
+
     return 0;
 }
